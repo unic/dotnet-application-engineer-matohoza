ab12ae12d0707a065dfb86608352abad2b2fb494
Martin <mato.hoza@gmail.com>
2019-02-04 01:59:55 +0100

    die lösung

---
+++ assignment/Controllers/RenamingController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment.Controllers
+{
+    public class RenamingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(DefaultModel model)
+        {
+            return View(nameof(Index), model);
+        }
+    }
+}
---
+++ assignment/Models/DefaultModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment.Models
+{
+    public class DefaultModel
+    {
+        public string Name { get; set; }
+
+        public string Greeting
+        {
+            get { return string.IsNullOrEmpty(Name) ? "" : $"Hallo {Name}!"; }
+        }
+    }
+}
